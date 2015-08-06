68fcef0b579a2d784bc177cd52ccb147d3b1d01e
Yusuke Watanabe <yusukew052@gmail.com>
2015-08-07 03:31:47 +0900

    update to validate filtering rules and show errors

--- README.md
+++ README.md
@@ -3,9 +3,23 @@ A Chrome extension to check a message before sending in Slack.
 
 ## Installation
 
-### Chrome Web Store
+#### Chrome Web Store
 
-https://chrome.google.com/webstore/detail/slack-message-checker/bpbfbabfppmapfaimkbkgdnmgolbpoei?hl=ja&gl=JP
+Go to [Chrome Web Store](https://chrome.google.com/webstore/detail/slack-message-checker/bpbfbabfppmapfaimkbkgdnmgolbpoei?hl=ja&gl=JP) and install.
+
+#### Source Code
+
+1. Clone source code
+
+  ```
+  git clone git@github.com:nabeix/slack-message-checker.git
+  ```
+2. Go to the extension settings page in Google Chrome
+3. Enable "Developer mode" and install this extension from "Load unpacked extension..." button
+
+## Usage
+
+Visit Slack Message Checker option page from [chrome://extensions](chrome://extensions/) and make filtering rules.
 
 ## Lisence
 
--- js/options.js
+++ js/options.js
@@ -10,74 +10,110 @@ $(function() {
     }
   };
 
-  chrome.storage.sync.get(
-    "data", function(saved) {
-      var data = saved.data;
-      if (!data) {
-        data = {
-          general: [],
-          teams: []
-        };
+  initialize();
+
+  function initialize() {
+    $("#team-dialog").hide();
+    $("#info").hide();
+    $("#new-team").bind("click", openNewTeamDialog);
+    $("#dialog-ok").bind("click", addNewTeam);
+    $("#save-btn").bind("click", saveRules);
+    $("#dialog-cancel").bind("click", closeNewTeamDialog);
+    $("#team-text-input").bind("keydown", function(e) {
+      if (e.keyCode === 13) {
+        addNewTeam();
       }
-      vm = new Vue({
-        el: "#main",
-        data: data,
-        methods: {
-          deleteGeneralFilter: function(index) {
-            vm.general.$remove(index);
-          },
-          deleteTeamFilter: function(teamIndex, filterIndex) {
-            vm.teams[teamIndex].filters.$remove(filterIndex);
-          },
-          addTeamFilter: function(teamIndex) {
-            var f = $.extend(true, {}, filterTemplate);
-            vm.teams[teamIndex].filters.push(f);
-          },
-          addGeneralFilter: function() {
-            var f = $.extend(true, {}, filterTemplate);
-            vm.general.push(f);
-          },
-          deleteTeam: function(teamIndex) {
-            vm.teams[teamIndex].$delete("filters");
-            vm.teams.$remove(teamIndex);
+    });
+    chrome.storage.sync.get(
+      "data", function(saved) {
+        var data = saved.data;
+        if (!data) {
+          data = {general: [], teams: []};
+        }
+        initVue(data);
+      }
+    );
+  }
+
+  function initVue(data) {
+    vm = new Vue({
+      el: "#main",
+      data: data,
+      filters: {
+        keywordValidator: {
+          write: function (val) {
+            var matchType = $(this.$el).find("input[type=radio]:checked").val();
+            validateKeyword(val, matchType, $(this.$el).find(".error"));
+            return val;
+          }
+        },
+        matchTypeValidator: {
+          write: function (val) {
+            var keyword = $(this.$el).find(".keyword").val();
+            validateKeyword(keyword, val, $(this.$el).find(".error"));
+            return val;
           }
         }
-      });
+      },
+      methods: {
+        deleteGeneralFilter: function(index) {
+          vm.general.$remove(index);
+        },
+        deleteTeamFilter: function(teamIndex, filterIndex) {
+          vm.teams[teamIndex].filters.$remove(filterIndex);
+        },
+        addTeamFilter: function(teamIndex) {
+          var f = $.extend(true, {}, filterTemplate);
+          vm.teams[teamIndex].filters.push(f);
+        },
+        addGeneralFilter: function() {
+          var f = $.extend(true, {}, filterTemplate);
+          vm.general.push(f);
+        },
+        deleteTeam: function(teamIndex) {
+          vm.teams[teamIndex].$delete("filters");
+          vm.teams.$remove(teamIndex);
+        }
+      }
+    });
+  }
+
+  function validateKeyword(keyword, matchType, errorElem) {
+    errorElem.text("");
+    if (keyword.length === 0) {
+      errorElem.text("ERROR: keyword is empty");
+      return false;
     }
-  );
+    if (matchType === "regexp") {
+      try {
+        var regexp = new RegExp(keyword);
+      } catch (e) {
+        errorElem.text("ERROR: invalid regular expression");
+        return false;
+      }
+    }
+    return true;
+  }
 
-  $("#team-dialog").hide();
-
-  $("#save-btn").bind("click", function() {
-    var data = JSON.parse(JSON.stringify(vm.$data));
-    chrome.storage.sync.set({data: data});
-  });
-
-  $("#new-team").bind("click", function() {
+  function openNewTeamDialog() {
     $("#team-dialog").show();
     $("#dialog-error").text("");
     $("#team-text-input").val("").focus();
-  });
+  }
 
-  $("#team-text-input").bind("keydown", function(e) {
-    if (e.keyCode === 13) {
-      $("#dialog-ok").trigger("click");
-    }
-  });
-
-  $("#dialog-cancel").bind("click", function() {
+  function closeNewTeamDialog() {
     $("#team-dialog").hide();
-  });
+  }
 
-  $("#dialog-ok").bind("click", function() {
+  function addNewTeam() {
     var team = $("#team-text-input").val();
     if (!/^[-a-z0-9]+?$/.test(team)) {
-      $("#dialog-error").text("Error: invalid team name").show();
+      $("#dialog-error").text("ERROR: invalid team name").show();
       return;
     }
     for (var i = 0; i < vm.teams.length; i++) {
       if (vm.teams[i].name === team) {
-        $("#dialog-error").text("Error: team name already exists").show();
+        $("#dialog-error").text("ERROR: team name already exists").show();
         return;
       }
     }
@@ -85,5 +121,29 @@ $(function() {
     filters.push($.extend(true, {}, filterTemplate));
     vm.teams.push({name: team, filters: filters});
     $("#team-dialog").hide();
-  });
+  }
+
+  function requestHideInfo() {
+    setTimeout(function() {
+      $("#info").hide();
+    }, 3500);
+  }
+
+  function saveRules() {
+    var error = false;
+    $(".rule").each(function(index, elem) {
+      var t = $(elem);
+      var r = validateKeyword(t.find(".keyword").val(), t.find("input[type=radio]:checked").val(), t.find(".error"));
+      error = !error && r ? false : true;
+    });
+    if (error) {
+      $("#info").text("Failed to save your changes. Please check error messages.").removeClass("info-success").addClass("info-error").show();
+      return;
+    } else {
+      $("#info").text("Your changes have been saved.").removeClass("info-error").addClass("info-success").show();
+      requestHideInfo();
+    }
+    var data = JSON.parse(JSON.stringify(vm.$data));
+    chrome.storage.sync.set({data: data});
+  }
 });
